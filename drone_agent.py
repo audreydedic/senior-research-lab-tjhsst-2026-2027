0f993601454f601db08297b0d3502d0df8572c52
audreydedic <dedickina@gmail.com>
2026-09-09 09:42:37 -0400

    update

---
+++ drone_agent.py
@@ -0,0 +1,14 @@
+from djitellopy import tello
+import pybullet as p
+import pybullet_data
+import time
+
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("/assets/racer.urdf",startPos, startOrientation)
